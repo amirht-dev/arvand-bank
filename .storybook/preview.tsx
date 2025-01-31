7492468c5140b4dcc8340fcac1b2674d72ee3ae4
amirhossein taheri <amirht.dev@gmail.com>
2025-01-31 18:30:33 +0330

    add font to storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import { iranSansX } from "@/lib/fonts";
+import { Preview } from "@/types/storybook";
+import "../src/styles/globals.css";
+
+const preview: Preview = {
+  tags: ["autodocs"],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className={`${iranSansX.variable} font-iranSansX antialiased`}>
+        {Story()}
+      </div>
+    ),
+  ],
+};
+
+export default preview;
