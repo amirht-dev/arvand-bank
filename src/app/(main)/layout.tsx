33eb51e187ce87578024cf94a8109c06a4422df8
amirhossein taheri <amirht.dev@gmail.com>
2025-02-06 15:04:25 +0330

    add Sidebar to main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/organisms/Header";
+import Sidebar from "@/components/organisms/Sidebar";
 
 export default function MainLayout({ children }: NextLayoutProps) {
   return (
-    <div>
+    <div className="flex min-h-dvh flex-col">
       <Header />
-      {children}
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 px-10 py-8">{children}</main>
+      </div>
     </div>
   );
 }
