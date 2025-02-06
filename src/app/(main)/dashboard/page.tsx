e27f5eba9c27a8f774b3482d86d8cd3a7c4ec8c5
amirhossein taheri <amirht.dev@gmail.com>
2025-02-06 16:38:47 +0330

    create Cards dashboard section

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,12 @@
+import IconButton from "@/components/atoms/buttons/IconButton";
 import { ChartLineUp_Outline } from "@/components/atoms/icons/Charts&Diagram/ChartLineUp";
+import { FileExport_Outline } from "@/components/atoms/icons/Files/FileExport";
+import { PlusSquare_Outline } from "@/components/atoms/icons/general/PlusSquare";
+import { CreditCartEdit_Outline } from "@/components/atoms/icons/Money&Payment/CreditCartEdit";
+import Card from "@/components/molecules/Card";
+import { ComponentPropsWithoutRefWithProps } from "@/types/utils";
+import { cn } from "@/utils/utils";
+import { Arrayable } from "type-fest";
 
 export default function DashboardPage() {
   return (
@@ -7,6 +15,62 @@ export default function DashboardPage() {
         <ChartLineUp_Outline className="size-8" />
         <span className="text-heading-4">داشبورد</span>
       </h4>
+
+      <Cards />
     </>
+  );
+}
+
+const Segment = ({
+  title,
+  actions,
+  className,
+  children,
+  ...props
+}: ComponentPropsWithoutRefWithProps<
+  "div",
+  {
+    title: string;
+    actions?: Arrayable<React.ReactNode>;
+  }
+>) => {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "space-y-6 rounded-2xl border border-neutral-gray-9 p-6",
+        className,
+      )}
+    >
+      <div className="flex items-center justify-between">
+        <h5 className="text-heading-5 text-neutral-gray-1">{title}</h5>
+
+        {actions && <div className="flex gap-2">{actions}</div>}
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+function Cards() {
+  return (
+    <Segment
+      className="mt-8"
+      title="کارت‌ها"
+      actions={[
+        <IconButton key="add-new-card">
+          <PlusSquare_Outline className="size-6" />
+        </IconButton>,
+        <IconButton key="edit-card">
+          <CreditCartEdit_Outline className="size-6" />
+        </IconButton>,
+        <IconButton key="export">
+          <FileExport_Outline className="size-6" />
+        </IconButton>,
+      ]}
+    >
+      <Card />
+    </Segment>
   );
 }
