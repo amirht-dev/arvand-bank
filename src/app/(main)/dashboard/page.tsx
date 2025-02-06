96951654c1030497fcf51e163d5d84421a02a4d9
amirhossein taheri <amirht.dev@gmail.com>
2025-02-06 15:06:09 +0330

    initiate dashboard page with title

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { ChartLineUp_Outline } from "@/components/atoms/icons/Charts&Diagram/ChartLineUp";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <h4 className="flex items-center gap-2 text-primary-700">
+        <ChartLineUp_Outline className="size-8" />
+        <span className="text-heading-4">داشبورد</span>
+      </h4>
+    </>
+  );
+}
