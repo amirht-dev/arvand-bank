41fac15dd8013dd98810e7fb5f94c4fde7858a07
amirhossein taheri <amirht.dev@gmail.com>
2025-02-09 12:24:06 +0330

    add Deposit section to account management page

--- src/app/(main)/account-management/page.tsx
+++ src/app/(main)/account-management/page.tsx
@@ -1,10 +1,40 @@
+import IconButton from "@/components/atoms/buttons/IconButton";
+import { FileExport_Outline } from "@/components/atoms/icons/Files/FileExport";
+import { PlusSquare_Outline } from "@/components/atoms/icons/general/PlusSquare";
+import { CreditCartEdit_Outline } from "@/components/atoms/icons/Money&Payment/CreditCartEdit";
 import { MoneyBills_Outline } from "@/components/atoms/icons/Money&Payment/MoneyBills";
+import Card from "@/components/molecules/Card";
 import PageTitle from "@/components/molecules/PageTitle";
+import Segment from "@/components/molecules/Segment";
 
 export default function AccountManagementPage() {
   return (
     <>
       <PageTitle icon={<MoneyBills_Outline />} label="مدیریت حساب" />
+
+      <div className="m-8"></div>
+      <Deposit />
     </>
+  );
+}
+
+function Deposit() {
+  return (
+    <Segment
+      title="سپرده"
+      actions={[
+        <IconButton key="add-new-card">
+          <PlusSquare_Outline className="size-6" />
+        </IconButton>,
+        <IconButton key="edit-card">
+          <CreditCartEdit_Outline className="size-6" />
+        </IconButton>,
+        <IconButton key="export">
+          <FileExport_Outline className="size-6" />
+        </IconButton>,
+      ]}
+    >
+      <Card />
+    </Segment>
   );
 }
