4a36e9a75928f8689127bf03f87e8a4761d32a48
amirhossein taheri <amirht.dev@gmail.com>
2025-02-09 11:42:00 +0330

    refactor main layout to only scroll main segment

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,11 +3,11 @@ import Sidebar from "@/components/organisms/Sidebar";
 
 export default function MainLayout({ children }: NextLayoutProps) {
   return (
-    <div className="flex min-h-dvh flex-col">
+    <div className="flex h-full max-h-dvh flex-col overflow-hidden">
       <Header />
-      <div className="flex flex-1">
+      <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 px-10 py-8">{children}</main>
+        <main className="flex-1 overflow-auto px-10 py-8">{children}</main>
       </div>
     </div>
   );
