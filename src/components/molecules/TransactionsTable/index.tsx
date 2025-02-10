c0c4d754e3d26e722acbabfeb3147ba6315bfa88
amirhossein taheri <amirht.dev@gmail.com>
2025-02-11 01:42:09 +0330

    add full version of transactions table

--- src/components/molecules/TransactionsTable/index.tsx
+++ src/components/molecules/TransactionsTable/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Chip from "@/components/atoms/Chip";
+import Pagination from "@/components/atoms/Pagination";
 import {
   Table,
   TableBody,
@@ -14,20 +15,28 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { useSearchParams } from "next/navigation";
 import { TransactionsTableProps } from "./index.types";
 
 type Transaction = {
-  type: "deposit" | "withdraw";
+  type: "deposit" | "withdraw" | "online-shopping";
   price: number;
   date: Date;
   status: "success" | "failed";
+  account: {
+    number: number;
+    holderName: string;
+  };
+  referenceNumber: number;
 };
 
 const TRANSACTION_TYPE_MAP: Record<Transaction["type"], string> = {
   deposit: "واریز",
   withdraw: "برداشت",
+  "online-shopping": "خرید اینترنتی",
 };
 
 const columnHelper = createColumnHelper<Transaction>();
@@ -58,22 +67,65 @@ const columns = [
   }),
 ];
 
-const data = Array.from<Transaction, Transaction>({ length: 3 }, () => ({
+const totalColumns = [
+  ...columns,
+  columnHelper.accessor("account.number", {
+    header: "شماره حساب",
+    cell: (transaction) => transaction.getValue(),
+  }),
+  columnHelper.accessor("account.holderName", {
+    header: "نام دارنده حساب",
+    cell: (transaction) => transaction.getValue(),
+  }),
+  columnHelper.accessor("referenceNumber", {
+    header: "شماره رجاع",
+    cell: (transaction) => transaction.getValue(),
+  }),
+];
+
+const data = Array.from<Transaction, Transaction>({ length: 20 }, () => ({
   type: "deposit",
   price: 311_440_000,
   date: new Date(),
   status: "success",
+  account: {
+    number: 43215987263,
+    holderName: "محمدرضا قاسمی",
+  },
+  referenceNumber: 12345678,
 }));
 
 const TransactionsTable = ({ simple = false }: TransactionsTableProps) => {
+  const pageIndex = parseInt(useSearchParams().get("page") ?? "0");
+
   const table = useReactTable({
-    columns,
-    data,
+    columns: simple ? columns : totalColumns,
+    data: simple ? data.slice(0, 3) : data,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: simple ? undefined : getPaginationRowModel(),
+    initialState: {
+      columnOrder: simple
+        ? undefined
+        : [
+            "type",
+            "account_number",
+            "account_holderName",
+            "price",
+            "date",
+            "referenceNumber",
+            "status",
+          ],
+    },
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize: 6,
+      },
+    },
   });
 
-  if (simple)
-    return (
+  return (
+    <>
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((group) => (
@@ -101,7 +153,21 @@ const TransactionsTable = ({ simple = false }: TransactionsTableProps) => {
           ))}
         </TableBody>
       </Table>
-    );
+      {table.getPageCount() > 1 && (
+        <Pagination
+          currentPage={table.getState().pagination.pageIndex + 1}
+          pages={table.getPageCount()}
+          prevAndNext
+          prevDisabled={!table.getCanPreviousPage()}
+          nextDisabled={!table.getCanNextPage()}
+          linkFormatter={(page) => `?page=${page - 1}`}
+          linkProps={{
+            scroll: false,
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default TransactionsTable;
